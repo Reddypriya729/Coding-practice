9b8eaaa1636b33e58cd3c8368841899ca6f400aa
Reddypriya729 <24B11DS070@adityauniversity.in>
2025-10-15 10:21:10 +0530

    Add files via upload

---
+++ function overloading.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class print{
+public:
+	void show(int x){
+		cout <<"Integer:"<<x<<endl;
+	}
+	void show(double x){
+		cout<<"Double:"<<x<<endl;
+	}
+	void show (string x){
+		cout<<"string:"<<x<<endl;
+	}
+};
+int main(){
+	print p;
+	p.show(7);
+	p.show(2.9);
+	p.show("Hello");
+	return 0;
+}
